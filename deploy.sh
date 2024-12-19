69615515b14d28ec1a29faddae0e6977c17005ab
karin <ovbkarin@gmail.com>
2024-12-18 23:47:34 -0500

    updated deploy master update

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 Developers at work
 Application is ready to be deployed to the development environment. 
 All deployment must be reviewed by managers before deployment
+AWS code deploy is great tool
